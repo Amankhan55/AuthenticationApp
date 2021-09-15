869b8b5f17c0f55ea0da4cf0fc7fb45ed2335d8a
Aman <amann.khan58@gmail.com>
2021-09-15 19:17:32 +0530

    Save user api completed

---
+++ models/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if (!err) {
+        console.log('MongoDB connection succeeded...');
+    } else {
+        console.log('Error in mongoDB conncetion: ' + JSON.stringify(err, undefined, 2));
+    }
+});
+
+require('../models/user.model');
